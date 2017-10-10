ff922309530012c41259a9a6b9363f0903c25d19
Y0haa <Eyoha.g@gmail.com>
2017-10-10 17:35:55 -0400

    new added groups

---
+++ app/src/main/java/com/example/admin/chatterbox/view/joingroup/JoinGroup.java
@@ -0,0 +1,8 @@
+package com.example.admin.chatterbox.view.joingroup;
+
+/**
+ * Created by Admin on 10/10/2017.
+ */
+
+public class JoinGroup {
+}
---
+++ app/src/main/java/com/example/admin/chatterbox/view/loginactivity/LoginActivity.java
@@ -0,0 +1,8 @@
+package com.example.admin.chatterbox.view.loginactivity;
+
+/**
+ * Created by Admin on 10/10/2017.
+ */
+
+public class LoginActivity {
+}
