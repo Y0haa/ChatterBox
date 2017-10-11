bb0b44adbb1a29e62ae5b9153714b1dc15628a1b
sdsearle <spencer.seiro@gmail.com>
2017-10-11 19:21:14 -0400

    added fire base

--- app/src/main/java/com/example/admin/chatterbox/view/mainactivity/MainActivityPresenter.java
+++ app/src/main/java/com/example/admin/chatterbox/view/mainactivity/MainActivityPresenter.java
@@ -1,15 +1,21 @@
 package com.example.admin.chatterbox.view.mainactivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * Created by Admin on 10/10/2017.
  */
 
-public class MainActivityPresenter implements MainActivityContract.Presenter{
+public class MainActivityPresenter implements MainActivityContract.Presenter {
 
     MainActivityContract.View view;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
 
     @Override
-    public void attacheView(MainActivityContract.View view) { this.view = view;
+    public void attacheView(MainActivityContract.View view) {
+        this.view = view;
 
     }
 
@@ -22,7 +28,7 @@ public class MainActivityPresenter implements MainActivityContract.Presenter{
     @Override
     public void validationInput(String inputString) {
 
-        String outputString ="Test" + inputString;
+        String outputString = "Test" + inputString;
 
         view.updateView(outputString);
 
